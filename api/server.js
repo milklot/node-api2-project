a850cdaaf26611316d4489c3bdc3d5558b6e3734
milklot <vbiostflex@gmail.com>
2021-04-10 20:56:30 -0400

    mvp

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const postsRouter = require('./posts/posts-router');
+
+const server = express();
+
+server.use(express.json());
+server.use(postsRouter);
+
+server.get('/', (req, res) => {
+	res.json({
+		message: "server up and running"
+	})
+});
+
+module.exports = server;
